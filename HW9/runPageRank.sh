b1c86350221195df1730539b8d3de1f394793a98
arn25-lk <arnavb25@gmail.com>
2023-05-02 17:25:21 -0400

    fixed directories

--- HW9/runPageRank.sh
+++ HW9/runPageRank.sh
@@ -1,3 +1,3 @@
 javac -Xlint -cp src/:lib/kvs.jar:lib/webserver.jar:lib/flame.jar src/cis5550/jobs/PageRank.java
-jar  cvf lib/pagerank.jar src/cis5550/jobs/PageRank.class
-java -cp lib/kvs.jar:lib/webserver.jar:lib/flame.jar cis5550.flame.FlameSubmit localhost:9000 lib/pagerank.jar cis5550.jobs.PageRank 0.01
+jar  cvf ../lib/pagerank.jar cis5550/jobs/PageRank.class
+# java -cp lib/kvs.jar:lib/webserver.jar:lib/flame.jar cis5550.flame.FlameSubmit localhost:9000 lib/pagerank.jar cis5550.jobs.PageRank 0.01
--- HW9/runScript.sh
+++ HW9/runScript.sh
@@ -1,3 +1,3 @@
-javac -Xlint -cp src/:lib/kvs.jar:lib/webserver.jar:lib/flame.jar src/cis5550/jobs/Indexer.java
-jar  cvf lib/indexer.jar src/cis5550/jobs/Indexer.class
-java -cp lib/kvs.jar:lib/webserver.jar:lib/flame.jar cis5550.flame.FlameSubmit localhost:9000 lib/indexer.jar cis5550.jobs.Indexer
+javac -Xlint -cp src/:lib/kvs.jar:lib/webserver.jar:lib/flame.jar cis5550/jobs/Indexer.java
+jar  cvf ../lib/Indexer.jar cis5550/jobs/Indexer.class
+#java -cp lib/kvs.jar:lib/webserver.jar:lib/flame.jar cis5550.flame.FlameSubmit localhost:9000 lib/indexer.jar cis5550.jobs.Indexer
